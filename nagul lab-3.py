297ac9d53fee660682f79a616ac1325f5387e89a
nagularavindhan08-hub <nagularavindhan08@gmail.com>
2026-06-04 16:58:04 +0530

    Add files via upload

---
+++ nagul lab-3.py
@@ -0,0 +1,66 @@
+#1)
+for i in range(1, 11):
+    print(i)
+#2)
+num = int(input("Enter a number: "))
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+#3)
+for i in range(2, 51, 2):
+    print(i)
+#4)
+num = int(input("Enter a number: "))
+factorial = 1
+for i in range(1, num + 1):
+    factorial *= i
+print(f"Factorial is: {factorial}")
+#5)
+for i in range(65, 72):
+    print(chr(i))
+#6)
+for i in range(1, 5):
+    print("*" * i)
+#7)
+for i in range(1, 21, 2):
+    print(i)
+#8)
+size = 5
+for i in range(size):
+    if i == 0 or i == size - 1:
+        print("*" * size)
+    else:
+        print("*" + " " * (size - 2) + "*")
+#9)
+for i in range(4, 0, -1):
+    print("*" * i)
+#10)
+rows = 4
+current_num = 1
+for i in range(1, rows + 1):
+    for j in range(i):
+        print(current_num, end=" ")
+        current_num += 1
+    print()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    

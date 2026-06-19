6e54f991028b538b1a1a30110bb4f86bfbb98c04
nagularavindhan08-hub <nagularavindhan08@gmail.com>
2026-06-19 17:18:22 +0530

    Add files via upload

---
+++ nagul lab 5.py
@@ -0,0 +1,197 @@
+'''#1
+for i in range(1, 21):
+    print(i)
+    if i == 10:
+        break
+#2
+for i in range(1, 21):
+    print(i)
+    if i == 10:
+        break
+#3
+for _ in range(100):
+    num = int(input("Enter a number: "))
+    if num == 0:
+        break
+#4
+for _ in range(100):
+    num = int(input("Enter a number: "))
+    if num == 0:
+        break
+#5
+for i in range(1, 101):
+    print(i)
+    if i % 17 == 0:
+        break
+#6
+while True:
+    print("1. Add")
+    print("2. View")
+    print("3. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "3":
+        break
+#7
+for _ in range(100):
+    text = input("Enter text: ")
+    if text.lower() == "quit":
+        break
+#8
+numbers = [1, 3, 5, 8, 10]
+
+for num in numbers:
+    if num % 2 == 0:
+        print("First even number:", num)
+        break
+#9
+for i in range(1, 21):
+    if i == 10:
+        continue
+    print(i)
+
+#10
+for i in range(1,50):
+    if i%2==0:
+        continue
+    print(i)
+#11
+for i in range(1,50):
+    if i%2!=0:
+        continue
+    print(i)
+#11
+for i in range(1, 101):
+    if i % 5 == 0:
+        continue
+    print(i)
+#12
+name="Nihanth"
+for letters in name:
+    if letters.lower() in "aeiou":
+        continue
+    print(letters)
+#13
+for i in range(1, 31):
+    if i % 3 == 0:
+        continue
+    print(i)
+#14
+numbers = [5, -2, 8, -1, 10]
+
+for num in numbers:
+    if num < 0:
+        continue
+    print(num)
+#15
+for i in range(1, 21):
+    if i % 2 == 0:
+        continue
+    print(i)
+#16
+def student_details():
+    pass
+#17
+class employee:
+    pass
+#18
+count=1
+while count<=5:
+    pass
+    break
+#19
+for i in range(5):
+    pass
+#20
+def calculator():
+    pass
+#21
+class BankAccount:
+    pass'''
+#22
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

b9cc16a33de5dcad115ab4a3363b16561b5764c5
nagularavindhan08-hub <nagularavindhan08@gmail.com>
2026-06-23 17:12:36 +0530

    Add files via upload

---
+++ nagul lab 6.2.py
@@ -0,0 +1,106 @@
+#1
+students = ["Arun", "Bala", "Kavin", "Ravi", "Siva"]
+print(students)
+#2
+students = ["Arun", "Bala", "Kavin"]
+students.append("Ravi")
+print(students)
+#3
+students = ["Arun", "Bala", "Kavin"]
+students.append("Ravi")
+print(students)
+#4
+students=["arun","bala","kavin",]
+students.remove("bala")
+print(students)
+#5
+students = ["Arun", "Bala", "Kavin", "Ravi"]
+print(len(students))
+#6
+students = ["Ravi", "Arun", "Kavin", "Bala"]
+students.sort()
+print(students)
+#7
+students = ["Arun", "Bala", "Kavin"]
+students.reverse()
+print(students)
+#8
+numbers = [10, 45, 23, 67, 34]
+print(max(numbers))
+#9
+numbers = [10, 20, 10, 30, 10]
+print(numbers.count(10))
+#10
+list1 = [1, 2, 3, 4]
+list2 = list1.copy()
+
+print("Original:", list1)
+print("Copy:", list2)
+#11
+t = (10, 20, 30, 40, 50)
+print(t)
+#12
+t = (10, 20, 30, 40, 50)
+print(len(t))
+#13
+t = (10, 20, 30, 40, 50)
+print("First:", t[0])
+print("Last:", t[-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

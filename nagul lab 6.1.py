6308e33f445c7ebb457746b5e71132c0aa168c27
nagularavindhan08-hub <nagularavindhan08@gmail.com>
2026-06-23 16:45:41 +0530

    Add files via upload

---
+++ nagul lab 6.1.py
@@ -0,0 +1,117 @@
+#1
+name = input("nagul: ")
+print(name.upper())
+#2
+sentence = input("MY NAME IS NAGUL: ")
+print(sentence.lower())
+#3
+sentence = input("Enter a sentence: ")
+print(sentence.capitalize())
+#4
+name = input("nishanth: ")
+age = input("17: ")
+
+print("My name is {} and I am {} years old.".format(name, age))
+#5
+word = input("vipul: ")
+char = input("u: ")
+
+print("Index:", word.index(char))
+#6
+sentence = input("nishanth is a boy: ")
+substring = input("nishanth: ")
+
+print("Position:", sentence.find(substring))
+#7
+word = input("finding: ")
+
+print(word.endswith("ing"))
+#8
+text = "Name\tAge\tCity"
+
+print("Original:")
+print(text)
+
+print("\nAfter expandtabs():")
+print(text.expandtabs(15))
+#9
+text = input("Enter a string: ")
+
+print(text.encode("utf-8"))
+#10
+text = input("Enter a string: ")
+
+print(text.isdigit())
+#11
+text = input("Enter a string: ")
+
+print(text.isnumeric())
+#12
+text = input("Enter a string: ")
+
+print(text.isalnum())
+#13
+text = input("Enter a string: ")
+
+print(text.isascii())
+#14
+text = input("Enter a string: ")
+
+print(text.isalpha())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
